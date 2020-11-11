f1e8f7a81b827a3fcfb948b7400f6fb6cc34e17e
gabriele.lana <gabriele.lana@gmail.com>
2020-11-11 10:56:47 +0100

    Add ping route to check if it works

--- main.go
+++ main.go
@@ -10,6 +10,10 @@ import (
 func main() {
 	router := gin.Default()
 
+	router.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong!")
+	})
+
 	router.GET("/n", func(c *gin.Context) {
 		n := 42
 		c.String(http.StatusOK, fmt.Sprint(n))
